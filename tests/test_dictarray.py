f3b60e8197e21ddabead6bb303afcf848b05a633
timjolson <tim.lsn@gmail.com>
2019-08-08 14:33:21 -0500

    Added comparison methods, improved indexing

--- tests/test_dictarray.py
+++ tests/test_dictarray.py
@@ -31,10 +31,10 @@ KEYS = {'x', 'y'}
 def check_equal(one, two):
     logging.debug('check_equal')
     logging.debug(f"keys {one._keys, two._keys}")
-    logging.debug(f"points {one.points, two.points}")
+    logging.debug(f"points {one.array, two.array}")
 
     assert set(one._keys) == set(two._keys)
-    assert np.all(one.points == two.points)
+    assert np.all(one.array == two.array)
 
 
 def test_base_constructor():
@@ -73,13 +73,13 @@ def test_constructor_point():
     for data in [dict1, ntup1, tup1, np1]:
         da = DictArray(data)
         assert data_type(da) == 'DictArray'
-        assert np.all(da.points == VALUES1)
+        assert np.all(da.array == VALUES1)
         assert set(da.keys()) == KEYS
 
     for data in [dict2, ntup2, tup2, np2]:
         da = DictArray(data)
         assert data_type(da) == 'DictArray'
-        assert np.all(da.points == VALUES2)
+        assert np.all(da.array == VALUES2)
         assert set(da.keys()) == KEYS
 
 
@@ -89,11 +89,12 @@ def test_constructor_points():
     for data in [dict3, ntup3, tup3, np3]:
         da = DictArray(data)
         assert data_type(da) == 'DictArray'
-        assert np.all(da.points == VALUES3)
+        assert np.all(da.array == VALUES3)
         assert set(da.keys()) == KEYS
 
 
 def test_array_read_ops():
+    logging.debug('---------------Begin test_array_read_ops()')
     Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
     da = DictArray(Dict)
 
@@ -131,6 +132,7 @@ def test_array_read_ops():
 
 
 def test_array_set_ops():
+    logging.debug('---------------Begin test_array_set_ops()')
     Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
     ds_constructed = DictArray(Dict)
 
@@ -142,11 +144,11 @@ def test_array_set_ops():
     da['y'] = Dict['y']
     assert np.all(ds_constructed['y'] == da['y'])
 
-    assert np.all(ds_constructed.points == da.points) == False  # 'is' doesn't want to work here
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
 
     da['z'] = Dict['z']
     assert np.all(ds_constructed['z'] == da['z'])
-    assert np.all(ds_constructed.points == da.points)
+    assert np.all(ds_constructed.array == da.array)
 
     # [key, :] =
     da = DictArray({'x':[0,0],'y':[0,0],'z':[0,0]})
@@ -156,11 +158,11 @@ def test_array_set_ops():
     da['y',:] = Dict['y']
     assert np.all(ds_constructed['y'] == da['y'])
 
-    assert np.all(ds_constructed.points == da.points) == False  # 'is' doesn't want to work here
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
 
     da['z',:] = Dict['z']
     assert np.all(ds_constructed['z'] == da['z'])
-    assert np.all(ds_constructed.points == da.points)
+    assert np.all(ds_constructed.array == da.array)
 
     # [key][:] =
     da = DictArray({'x':[0,0],'y':[0,0],'z':[0,0]})
@@ -170,9 +172,123 @@ def test_array_set_ops():
     da['y'][:] = Dict['y']
     assert np.all(ds_constructed['y'] == da['y'])
 
-    assert np.all(ds_constructed.points == da.points) == False  # 'is' doesn't want to work here
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
 
     da['z'][:] = Dict['z']
     assert np.all(ds_constructed['z'] == da['z'])
-    assert np.all(ds_constructed.points == da.points)
+    assert np.all(ds_constructed.array == da.array)
+
+
+def test_getitem_multiple():
+    logging.debug('---------------Begin test_getitem_multiple()')
+    Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
+
+    da = DictArray(Dict)
+    logging.debug(da[['x','y']])
+    assert np.all(da[['x', 'y']] == np.vstack([da['x'], da['y']]))
+
+    with pytest.raises(IndexError):
+        da[0,('x', 'y')]
+
+    with pytest.raises(IndexError):
+        da[0,['x', 'y']]
+
+    with pytest.raises(IndexError):
+        da[('x', 'y')]
+
+    with pytest.raises(IndexError):
+        da[:, ('x', 'y')]
+
+
+def test_setitem_multiple():
+    logging.debug('---------------Begin test_setitem_multiple()')
+    Dict = {'x': [1., 4.], 'y': [2., 5], 'z': [3, 6]}
+
+    da = DictArray(Dict)
+    replace = np.vstack([da['y'], da['x']]).copy()
+    da[['x', 'y']] = replace
+    assert np.all(da[['x', 'y']] == replace)
+
+
+def test_object_keys():
+    logging.debug('---------------Begin test_object_keys()')
+    class thing(object):
+        pass
+    x, y, z = thing(), thing(), thing()
+    Dict = {x: [1., 4.], y: [2., 5], z: [3, 6]}
+    da = DictArray(Dict)
+
+    # check by point
+    assert np.all(da[:, 0] == [1., 2., 3])
+    assert np.all(da[:, 1] == [4., 5., 6])
+
+    # check x
+    assert np.all(da[x] == da[x, :])
+    assert np.all(da[x] == da[0, :])
+    assert np.all(da[x] == [1, 4])
+    assert da[x, 0] == 1 == da[x][0]
+    assert da[x, 1] == 4 == da[x][1]
+
+    # check y
+    assert np.all(da[y] == da[y, :])
+    assert np.all(da[y] == da[1, :])
+    assert np.all(da[y] == [2, 5])
+    assert da[y, 0] == 2 == da[y][0]
+    assert da[y, 1] == 5 == da[y][1]
+
+    # check z
+    assert np.all(da[z] == da[z, :])
+    assert np.all(da[z] == da[2, :])
+    assert np.all(da[z] == [3, 6])
+    assert da[z, 0] == 3 == da[z][0]
+    assert da[z, 1] == 6 == da[z][1]
+
+    with pytest.raises(IndexError):
+        print(da[0, z])
+    with pytest.raises(IndexError):
+        print(da[:, x])
+
+    ds_constructed = DictArray(Dict)
+
+    # [key] =
+    da = DictArray({x: [0, 0], y: [0, 0], z: [0, 0]})
+    da[x] = Dict[x]
+    assert np.all(ds_constructed[x] == da[x])
+
+    da[y] = Dict[y]
+    assert np.all(ds_constructed[y] == da[y])
+
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
+
+    da[z] = Dict[z]
+    assert np.all(ds_constructed[z] == da[z])
+    assert np.all(ds_constructed.array == da.array)
+
+    # [key, :] =
+    da = DictArray({x: [0, 0], y: [0, 0], z: [0, 0]})
+    da[x, :] = Dict[x]
+    assert np.all(ds_constructed[x] == da[x])
+
+    da[y, :] = Dict[y]
+    assert np.all(ds_constructed[y] == da[y])
+
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
+
+    da[z, :] = Dict[z]
+    assert np.all(ds_constructed[z] == da[z])
+    assert np.all(ds_constructed.array == da.array)
+
+    # [key][:] =
+    da = DictArray({x: [0, 0], y: [0, 0], z: [0, 0]})
+    da[x][:] = Dict[x]
+    assert np.all(ds_constructed[x] == da[x])
+
+    da[y][:] = Dict[y]
+    assert np.all(ds_constructed[y] == da[y])
+
+    assert np.all(ds_constructed.array == da.array) == False  # 'is' doesn't want to work here
+
+    da[z][:] = Dict[z]
+    assert np.all(ds_constructed[z] == da[z])
+    assert np.all(ds_constructed.array == da.array)
 
